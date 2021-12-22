ee8455bfd373337b52776bbf0d192e5a8c8d37a1
k-huggs <kyle_huggins@hotmail.co.uk>
2021-12-22 22:39:58 +0000

    seeding complete

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,6 +1,17 @@
-const db = require('../db/connection.js');
-const testData = require('../db/data/test-data/index.js');
-const { seed } = require('../db/seeds/seed.js');
+const db = require("../db/connection.js");
+const testData = require("../db/data/test-data/index.js");
+const seed = require("../db/seeds/seed.js");
+const request = require("supertest");
+const app = require("../app");
 
 beforeEach(() => seed(testData));
 afterAll(() => db.end());
+
+describe("GET /api/topics", () => {
+  test("status 200: returns an array of topics objects with the properties of slug and description", () => {
+    return request(app).get("api/topics").expect(200).then(( { body })=> 
+        console.log(result.rows);
+
+    })
+  });
+});
