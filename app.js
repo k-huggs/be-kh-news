e498ada317d19b57f6cdcde7cf4883df1c73b80c
Kyle Huggins <kylehuggins@Kyles-MacBook-Pro.local>
2022-12-14 09:48:48 +0000

    404 route error handled

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-const express = require("express");
-const apiRouter = require("./routers/apiRouter");
+const express = require('express');
+const apiRouter = require('./routers/apiRouter');
 const app = express();
 app.use(express.json());
 const {
@@ -7,13 +7,13 @@ const {
   handlePSQLErrors,
   handleRouteNotFoundErrors,
   handleServerErrors,
-} = require("./errors/errors");
+} = require('./errors/errors');
 
-app.use("/api", apiRouter);
+app.use('/api', apiRouter);
 
-app.use("*", handleCustomErrors);
+app.use('*', handleCustomErrors);
 app.use(handlePSQLErrors);
-app.use("/*", handleRouteNotFoundErrors);
+app.all('/*', handleRouteNotFoundErrors);
 app.use(handleServerErrors);
 
 module.exports = app;
